be13eed1e1cd6b2994ad4ea50e85491c0531eced
Diksha <diksha7726@gmail.com>
2026-08-22 09:44:50 +0530

    Add 4th Program

---
+++ Collection4.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Collection4 {
+    public static void main(String[] args){
+        ArrayList<String> program=new ArrayList<String>();
+        program.add("Python");
+        program.add("Dart");
+        program.add("Java");
+        program.add("Javascript");
+        System.out.println(program.contains("Java"));
+    }
+    
+}
