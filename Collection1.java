98a2a4d75a956354bbf203a7d251c8e435607ec2
Diksha <diksha7726@gmail.com>
2026-08-22 06:43:52 +0530

    Add Collection1 program

---
+++ Collection1.java
@@ -0,0 +1,16 @@
+import java.util.Collections;
+import java.util.ArrayList;
+public class Collection1 {
+    public static void main(String[] args){
+    ArrayList<String> country=new ArrayList<String>();
+    country.add("India");
+    country.add("Malaysia");
+    country.add("Singapore");
+    country.add("Dubai");
+    country.add("Qatar");
+    for(String c:country){
+        System.out.println(c);
+    }
+}   
+}
+
