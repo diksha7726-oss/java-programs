fc7089c643177c3d8713936c426ab0f45d928fb0
Diksha <diksha7726@gmail.com>
2026-08-26 18:49:43 +0530

    Add Linkedlist4 program

---
+++ Linkedlist/Linkedlist4.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+public class Linkedlist4 {
+    public static void main(String[] args){
+        LinkedList<String> name = new LinkedList<String>();
+        name.add("Diksha");
+        name.add("Priyu");
+        name.add("Anisha");
+        System.out.println(name.contains("Priyu"));
+    }    
+}
